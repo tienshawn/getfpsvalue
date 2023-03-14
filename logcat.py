e7e6ae515c21b7f3ddc55017966a70b26266bccb
tienshawn <tiennguyen14122003@gmail.com>
2023-03-14 22:21:15 +0700

    Change

--- logcat.py
+++ logcat.py
@@ -2,19 +2,25 @@ import re
 import pandas as pd
 import subprocess
 
-def connect_get_namespaced_pod_exec(output_file):
+def getlogvalues(output_file):
     command = "cat streamlog.log"
     output = subprocess.check_output(['/bin/bash', '-c', command])
     output2 = output.decode().split("\n")
     fps_values = []
+    bitrate_values = []
     for line in output2:
-        fps_match = re.search(r"fps=([\d.]+)", line)
-        if "fps" in line:
+        if "fps" and "bitrate" in line:
             fps = float(line.split('fps=')[1].split()[0])
             fps_values.append(fps)
-    
-    df = pd.DataFrame(fps_values, columns=["FPS value"])
+
+            bit = str(line.split('bitrate')[1].split()[0])
+            bitrate = ''.join(letter for letter in bit if letter.isnumeric() or letter == '.')
+            bitrate_values.append(float(bitrate))
+
+    data = {'FPS':fps_values, 
+            'Bitrate':bitrate_values}
+    df = pd.DataFrame(data)
     df.to_excel(output_file, index=False)
 
-output_file = "/home/dell/logpy.xlsx"
-connect_get_namespaced_pod_exec(output_file)
+output_file = "./logpy1.xlsx"
+getlogvalues(output_file)
